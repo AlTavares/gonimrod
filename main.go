8396f4ee1a5eed15afb75d63b04842600a9d2b0e
Alexandre Mantovani Tavares <alexandre@live.in>
2017-09-18 20:09:51 -0300

    feat: create command line tool

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+)
 
+func main() {
+	message := flag.String("m", "", "Message to send")
+	key := flag.String("k", "", "API Key")
+	flag.Parse()
+	nimrod := Nimrod{*key}
+	if len(*message) == 0 {
+		log.Fatalln("Message can't be empty")
+	}
+	err := nimrod.sendMessage(*message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Message sent")
 }
